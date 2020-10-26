c1b2fbb53206ce189a6eb457bce73a5b92a0f9fd
KavishkaY <64638298+KavishkaY@users.noreply.github.com>
2020-10-27 10:29:29 +1100

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style>
+footer {
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+</style>
 </head>
+
+
+
     <body>
 
 
