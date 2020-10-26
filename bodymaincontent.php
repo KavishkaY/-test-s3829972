0d8f868e2f8af56ba852a8595cdc2a80a117ae1c
KavishkaY <64638298+KavishkaY@users.noreply.github.com>
2020-10-27 10:26:59 +1100

    Update bodymaincontent.php

--- bodymaincontent.php
+++ bodymaincontent.php
@@ -5,6 +5,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <style>
+ {
+  float: left;
+  padding: 20px;
+  width: 70%;
+  background-color: #f1f1f1;
+  height: 300px; 
+}
+
+</style>
 </head>
     <body>
 
