3f67ed5d9575e5025af3e23157cb5f6e63966e77
KavishkaY <64638298+KavishkaY@users.noreply.github.com>
2020-10-27 10:35:27 +1100

    Update layout.php

--- layout.php
+++ layout.php
@@ -11,7 +11,7 @@
 
       <?php require_once ("rightheaderdiv.php"); ?>
       <?php require_once ("bodymaincontent.php"); ?>
-      <?php require_once ("sidebar.php.php"); ?>
+      <?php require_once ("sidebar.php"); ?>
       <?php require_once ("footer.php"); ?>
 
 
