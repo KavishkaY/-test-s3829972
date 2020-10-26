daf5b33115ee34c1e6015b68dc418b06e4329008
KavishkaY <64638298+KavishkaY@users.noreply.github.com>
2020-10-27 10:28:16 +1100

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+     {
+  float: right;
+  width: 30%;
+  height: 300px; 
+  background: #ccc;
+  padding: 20px;
+}
+</style>
+
+
+
+
+
+    
 </head>
     <body>
 
