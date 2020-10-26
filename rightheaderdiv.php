7661c4bff9288c0750d1e9a81345ab7fa0ff8436
KavishkaY <64638298+KavishkaY@users.noreply.github.com>
2020-10-27 10:27:45 +1100

    Update rightheaderdiv.php

--- rightheaderdiv.php
+++ rightheaderdiv.php
@@ -5,6 +5,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+ <style>
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+}
+</style>
 </head>
     <body>
 
