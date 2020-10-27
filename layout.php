a93a5ed3bc6d6c094e818144b34d72ef5dd3855f
KavishkaY <64638298+KavishkaY@users.noreply.github.com>
2020-10-27 11:13:51 +1100

    Update layout.php

--- layout.php
+++ layout.php
@@ -1,5 +1,3 @@
-<?php ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,10 +7,25 @@
     <body>
 
 
-      <?php require_once ("rightheaderdiv.php"); ?>
-      <?php require_once ("bodymaincontent.php"); ?>
-      <?php require_once ("sidebar.php"); ?>
-      <?php require_once ("footer.php"); ?>
+     <div style="  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;"> <?php require_once ("rightheaderdiv.php"); ?> </div>
+       <div style="   float: left;
+  padding: 20px;
+  width: 70%;
+  background-color: #000000;
+  height: 300px; "><?php require_once ("bodymaincontent.php"); ?></div>
+      <div style="   float: right;
+  width: 30%;
+  height: 300px; 
+  background: #ccc;
+  padding: 20px;"><?php require_once ("sidebar.php"); ?> </div>
+      <div style="   background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;"><?php require_once ("footer.php"); ?></div>
 
 
     </body>
