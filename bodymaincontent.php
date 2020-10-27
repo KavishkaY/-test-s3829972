23389c551433fca1739509634816b703850fc8ef
KavishkaY <64638298+KavishkaY@users.noreply.github.com>
2020-10-27 11:12:38 +1100

    Update bodymaincontent.php

--- bodymaincontent.php
+++ bodymaincontent.php
@@ -1,4 +1,4 @@
-<?php ?>
+<?php?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +11,27 @@
   float: left;
   padding: 20px;
   width: 70%;
-  background-color: #f1f1f1;
+  background-color: #000000;
   height: 300px; 
 }
 
 </style>
 </head>
     <body>
-
-
-
+        <div>
+            <?php
+                for ($x = 0; $x <= 2; $x++) {
+                    $breedsURL = 'https://dog.ceo/api/breed/hound/images/random/3' ;
+	                $dogs_json = file_get_contents($breedsURL);
+	                $dogs_array = json_decode($dogs_json, true);
+                    if(!empty($dogs_array)){
+                        foreach($dogs_array['message'] as $image){
+                            echo '<img src="'.$image.'" alt= ""/>';
+                        }
+                    }
+                  }
+                	
+            ?>
+        </div>    
     </body>
 </html>
